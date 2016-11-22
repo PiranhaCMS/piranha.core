f65b00d2d4f79e2e243fc9641f03c4ced90d269d
Billy Wolfington <man_of_magik903@yahoo.com>
2016-11-22 14:11:55 -0800

    Continued BlockTypeControllerUnitTest

--- test/core/Piranha.Manager.Tests/Areas/Manager/Controllers/BlockTypeControllerUnitTest.cs
+++ test/core/Piranha.Manager.Tests/Areas/Manager/Controllers/BlockTypeControllerUnitTest.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Piranha.Areas.Manager.Controllers;
 using Piranha.Extend;
 using Xunit;
@@ -19,9 +20,36 @@ namespace Piranha.Manager.Tests.Areas.Manager.Controllers
 {
     public class BlockTypeControllerUnitTest : ManagerAreaControllerUnitTestBase<BlockTypeController>
     {
+        #region Properties
+        #region Private Properties
+        private const int NUM_BLOCK_TYPES = 5;
+
+        private List<BlockType> blockTypes;
+        #endregion
+        #endregion
+
+        #region Test setup
+        protected override Mock<IApi> SetupApi() {
+            var api = new Mock<IApi>();
+            GenerateBlockTypes();
+            api.Setup(a => a.BlockTypes.Get()).Returns(blockTypes);
+            api.Setup(a => a.PageTypes.Get()).Returns(new List<PageType>());
+            return api;
+        }
+        private void GenerateBlockTypes() {
+            blockTypes = new List<BlockType>();
+            for (int i = 1; i <= NUM_BLOCK_TYPES; i++) {
+                blockTypes.Add(new BlockType {
+                    Id = i.ToString(),
+                    Title = "Block type " + i,
+                });
+            }
+        }
+
         protected override BlockTypeController SetupController() {
             return new BlockTypeController(mockApi.Object);
         }
+        #endregion
 
         [Fact]
         public void ListResultIsNotNull() {
@@ -35,8 +63,21 @@ namespace Piranha.Manager.Tests.Areas.Manager.Controllers
             #region Assert
             Assert.NotNull(result);
             IList<BlockType> Model = result.Model as IList<BlockType>;
-            Assert.NotNull(Model);
+            AssertBlockTypeListsMatches(Model);
             #endregion
+        }
+
+        private void AssertBlockTypeListsMatches(IList<BlockType> result) {
+            Assert.NotNull(result);
+            Assert.Equal(blockTypes.Count, result.Count);
+            for (int i = 0; i < blockTypes.Count; i++) {
+                AssertBlockTypesMatch(blockTypes[i], result[i]);
+            }
+        }
+        private void AssertBlockTypesMatch(BlockType expected, BlockType result)
+        {
+            Assert.Equal(expected.Id, result.Id);
+            Assert.Equal(expected.Title, result.Title);
         }
 
         [Fact]
