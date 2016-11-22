a50d3e7425ef5426f128118d8ed1247d2f07c58c
Billy Wolfington <man_of_magik903@yahoo.com>
2016-11-21 23:21:31 -0800

    Included references to corresponding Piranha project

---
+++ test/core/Piranha.Manager.Tests/Areas/Manager/Controllers/ManagerAreaControllerUnitTestBase.cs
@@ -0,0 +1,10 @@
+using Piranha.Areas.Manager.Controllers;
+
+namespace Piranha.Manager.Tests.Areas.Manager.Controllers
+{
+    public abstract class ManagerAreaControllerUnitTestBase<TController>
+        where TController : ManagerAreaControllerBase
+    {
+        
+    }
+}
