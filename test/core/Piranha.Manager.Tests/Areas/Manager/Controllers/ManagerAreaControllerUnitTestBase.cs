d26c125fb237d265943de2c61578fd3fd5137ec8
Billy Wolfington <man_of_magik903@yahoo.com>
2016-11-21 23:50:34 -0800

    Sample Controller Unit test
    Example unit test for a manager area controller written

--- test/core/Piranha.Manager.Tests/Areas/Manager/Controllers/ManagerAreaControllerUnitTestBase.cs
+++ test/core/Piranha.Manager.Tests/Areas/Manager/Controllers/ManagerAreaControllerUnitTestBase.cs
@@ -1,10 +1,35 @@
 using Piranha.Areas.Manager.Controllers;
+using Moq;
 
 namespace Piranha.Manager.Tests.Areas.Manager.Controllers
 {
     public abstract class ManagerAreaControllerUnitTestBase<TController>
         where TController : ManagerAreaControllerBase
     {
-        
+        #region Properties
+        #region Protected Properties
+        protected readonly TController _controller;
+
+        protected readonly Mock<IApi> _api;
+        #endregion
+        #endregion
+
+        #region Test initialize
+        public ManagerAreaControllerUnitTestBase()
+        {
+            _api = SetupApi();
+            _controller = SetupController();
+        }
+
+        protected virtual Mock<IApi> SetupApi()
+        {
+            return new Mock<IApi>();
+        }
+
+        protected virtual TController SetupController()
+        {
+            return new ManagerAreaControllerBase(_api.Object) as TController;
+        }
+        #endregion
     }
 }
