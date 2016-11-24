ff59524c2ec6ede503f29f2c695c3e8469e5f7e9
Billy Wolfington <man_of_magik903@yahoo.com>
2016-11-24 15:14:13 -0800

    Mocking page and page type data

--- test/data/Piranha.EF.Tests/Repositories/PageRepositoryUnitTest.cs
+++ test/data/Piranha.EF.Tests/Repositories/PageRepositoryUnitTest.cs
@@ -10,6 +10,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using Piranha.EF.Repositories;
@@ -19,24 +20,40 @@ namespace Piranha.EF.Tests.Repositories
     public class PageRepositoryUnitTest : RepositoryUnitTestBase<PageRepository>
     {
         #region Properties
+        private const int NUM_PAGE_TYPES = 3;
         private const int NUM_PAGES = 7;
 
-        private readonly List<Data.Page> pages = new List<Data.Page>();
-        private readonly Mock<IDataService> mockDataService = new Mock<IDataService>();
+        private IQueryable<Data.PageType> pageTypes;
+        private IQueryable<Data.Page> pages;
+        private readonly Mock<DbSet<Data.PageType>> mockPageTypeSet = new Mock<DbSet<Data.PageType>>();
         private readonly Mock<DbSet<Data.Page>> mockPagesSet = new Mock<DbSet<Data.Page>>();
+        
+        private readonly Mock<IDataService> mockDataService = new Mock<IDataService>();
         #endregion
 
         protected override void SetupMockDbData() {
+            CreateMockPageTypes();
             CreateMockPages();
             mockDb.Setup(db => db.Pages).Returns(mockPagesSet.Object);
         }
+        private void CreateMockPageTypes() {
+            List<Data.PageType> listOfPageTypes = new List<Data.PageType>();
+            for (int i = 0; i < NUM_PAGE_TYPES; i++) {
+                listOfPageTypes.Add(new Data.PageType {
+
+                });
+            }
+            pageTypes = listOfPageTypes.AsQueryable();
+        }
         private void CreateMockPages() {
+            List<Data.Page> listOfPages = new List<Data.Page>();
             for (int i = 0; i < NUM_PAGES; i++) {
-                pages.Add(new Data.Page {
+                listOfPages.Add(new Data.Page {
                     Id = ConvertIntToGuid(i + 1),
 
                 });
             }
+            pages = listOfPages.AsQueryable();
         }
 
         protected override PageRepository SetupRepository() {
