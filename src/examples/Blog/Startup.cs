e7784aee281e89f512f61a0b68bd380bea160d4c
Håkan Edling <hakan@tidyui.com>
2016-11-29 21:38:07 +0100

    Modified example seed

--- src/examples/Blog/Startup.cs
+++ src/examples/Blog/Startup.cs
@@ -88,25 +88,7 @@ namespace Blog
         /// </summary>
         /// <param name="db"></param>
         private void Seed(IApi api, Piranha.EF.Db db) {
-            if (db.Categories.Count() == 0) {
-                // Add the blog category
-                var category = new Piranha.EF.Data.Category() {
-                    Id = Guid.NewGuid(),
-                    Title = "Blog",
-                    ArchiveTitle = "Blog Archive"
-                };
-                db.Categories.Add(category);
-
-                // Add a post
-                var post = new Piranha.EF.Data.Post() {
-                    CategoryId = category.Id,
-                    Title = "My first post",
-                    Excerpt = "Etiam porta sem malesuada magna mollis euismod.",
-                    Body = "<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>",
-                    Published = DateTime.Now
-                };
-                db.Posts.Add(post);
-
+            if (api.Categories.Get().Count == 0) {
                 // Add the startpage
                 var startPage = Models.StartPageModel.Create("Start");
                 startPage.Title = "Welcome to Piranha CMS";
@@ -125,6 +107,23 @@ namespace Blog
                 startPage.Published = DateTime.Now;
                 api.Pages.Save(startPage);
 
+                // Add the blog category
+                var category = new Piranha.Models.Category() {
+                    Id = Guid.NewGuid(),
+                    Title = "Blog",
+                    ArchiveTitle = "Blog Archive"
+                };
+                api.Categories.Save(category);
+
+                // Add a post
+                var post = new Piranha.EF.Data.Post() {
+                    CategoryId = category.Id,
+                    Title = "My first post",
+                    Excerpt = "Etiam porta sem malesuada magna mollis euismod.",
+                    Body = "<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>",
+                    Published = DateTime.Now
+                };
+                db.Posts.Add(post);
                 db.SaveChanges();
             }
         }
