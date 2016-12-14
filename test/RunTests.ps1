257491ca272b399adc55be58e380415ae70da2ab
TheWolfA2 <man_of_magik903@yahoo.com>
2016-12-14 05:58:47 -0800

    Attempting a better test script

--- test/RunTests.ps1
+++ test/RunTests.ps1
@@ -1,50 +1,74 @@
-echo "Running test/core tests"
+$global:failedProjectCount = 0
+
+echo "`n`n"
+echo "build: Running test/core tests"
 foreach ($test in ls core/*.Tests) {
     Push-Location $test
 
     echo "Testing project in $test"
 
     dotnet restore
-    dotnet test
+    & dotnet test
+    if ($LASTEXITCODE -ne 0) {
+        $global:failedProjectCount += 1
+    }
 
     Pop-Location
 }
 
-echo ""
-echo "Running test/data tests"
+echo "`n`n"
+echo "build: Running test/data tests"
 foreach ($test in ls data/*.Tests) {
     Push-Location $test
 
     echo "Testing project in $test"
 
     dotnet restore
-    dotnet test
+    & dotnet test
+    if ($LASTEXITCODE -ne 0) {
+        $global:failedProjectCount += 1
+    }
 
     Pop-Location
 }
 
-echo ""
-echo "Running test/env tests"
+echo "`n`n"
+echo "build: Running test/env tests"
 foreach ($test in ls env/*.Tests) {
     Push-Location $test
 
     echo "Testing project in $test"
 
     dotnet restore
-    dotnet test
+    & dotnet test
+    if ($LASTEXITCODE -ne 0) {
+        $global:failedProjectCount += 1
+    }
 
     Pop-Location
 }
 
-echo ""
-echo "Running test/examples tests"
+echo "`n`n"
+echo "build: Running test/examples tests"
 foreach ($test in ls examples/*.Tests) {
     Push-Location $test
 
     echo "Testing project in $test"
 
     dotnet restore
-    dotnet test
+    & dotnet test
+    if ($LASTEXITCODE -ne 0) {
+        $global:failedProjectCount += 1
+    }
 
     Pop-Location
+}
+
+if ($global:failedProjectCount -ne 0)
+{
+    exit 1
+}
+else
+{
+    exit 0
 }
