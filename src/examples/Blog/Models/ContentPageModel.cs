a619d804a1f573c1ae1dfd0e238158b5ee5dc58e
Håkan Edling <hakan@tidyui.com>
2016-12-21 22:07:19 +0100

    Fixed page type in Blog example.

--- src/examples/Blog/Models/ContentPageModel.cs
+++ src/examples/Blog/Models/ContentPageModel.cs
@@ -7,18 +7,9 @@ namespace Blog.Models
     [PageType(Id = "Content", Title = "Content page")]
     public class ContentPageModel : Page<ContentPageModel>
     {
-        public class ContentPageSettings
-        {
-            [Field]
-            public ImageField PrimaryImage { get; set; }
-            [Field()]
-            public TextField Ingress { get; set; }
-
-        }
-
         [Region]
         public MarkdownField Body { get; set; }
         [Region]
-        public ContentPageSettings Settings { get; set; }
+        public TextField Ingress { get; set; }
     }
 }
