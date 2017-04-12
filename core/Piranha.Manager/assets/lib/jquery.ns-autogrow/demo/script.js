f67ab3bf3e172a95b67bcd0c734aaedc6f20f7ad
Håkan Edling <hakan@tidyui.com>
2017-04-12 21:44:48 +0200

    Deleted bower components from the repository.
    These should be fetched locally with bower install.

--- core/Piranha.Manager/assets/lib/jquery.ns-autogrow/demo/script.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $('.example1 textarea').autogrow();
-  $('.example2 textarea').autogrow({vertical: true, horizontal: false});
-  $('.example3 textarea').autogrow({vertical: false, horizontal: true});
-  $('.example4 textarea').autogrow({flickering: false});
-});
