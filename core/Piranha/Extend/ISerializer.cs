da448d6283152aff78ab5f7c275e6a8199b103cb
Håkan Edling <hakan@tidyui.com>
2017-04-27 22:57:28 +0200

    Added interface for custom serializers. See #86

---
+++ core/Piranha/Extend/ISerializer.cs
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016-2017 Håkan Edling
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ * 
+ * https://github.com/piranhacms/piranha.core
+ * 
+ */
+
+namespace Piranha.Extend
+{
+    /// <summary>
+    /// Interface for creating a field serializer.
+    /// </summary>
+    public interface ISerializer
+    {
+        /// <summary>
+        /// Serializes the given object.
+        /// </summary>
+        /// <param name="obj">The object</param>
+        /// <returns>The serialized value</returns>
+        string Serialize(object obj);
+
+        /// <summary>
+        /// Deserializes the given string.
+        /// </summary>
+        /// <param name="str">The serialized value</param>
+        /// <returns>The object</returns>
+        object Deserialize(string str);
+    }
+}
