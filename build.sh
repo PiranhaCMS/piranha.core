0c9a50d06968821771e455909f503b4532e47274
Håkan Edling <hakan@tidyui.com>
2017-05-06 05:58:46 +0200

    Simplified build even more

--- build.sh
+++ build.sh
@@ -3,12 +3,6 @@
 #exit if any command fails
 set -e
 
-artifactsFolder="./artifacts"
-
-if [ -d $artifactsFolder ]; then  
-  rm -R $artifactsFolder
-fi
-
 dotnet restore
 dotnet build
 dotnet test ./test/Piranha.Tests/Piranha.Tests.csproj
