817367e7c4dc3497342e4d1a5f5a9d6ab432d02c
Håkan Edling <hakan@tidyui.com>
2017-05-07 23:47:54 +0200

    Added config tests.

---
+++ test/Piranha.Tests/Config.cs
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2017 Håkan Edling
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ * 
+ * http://github.com/piranhacms/piranha
+ * 
+ */
+
+using Piranha.Extend;
+using System.Linq;
+using Xunit;
+
+namespace Piranha.Tests
+{
+    public class Config : BaseTests
+    {
+        /// <summary>
+        /// Sets up & initializes the tests.
+        /// </summary>
+        protected override void Init() {
+            using (var api = new Api(options, storage)) {
+                Piranha.App.Init(api);
+
+                using (var config = new Piranha.Config(api)) {
+                    config.CacheExpiresMedia = 0;
+                    config.CacheExpiresPages = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Cleans up any possible data and resources
+        /// created by the test.
+        /// </summary>
+        protected override void Cleanup() { }
+
+        [Fact]
+        public void CacheExpiresMedia() {
+            using (var api = new Api(options, storage)) {
+                using (var config = new Piranha.Config(api)) {
+                    Assert.Equal(0, config.CacheExpiresMedia);
+
+                    config.CacheExpiresMedia = 30;
+
+                    Assert.Equal(30, config.CacheExpiresMedia);
+                }
+            }
+        }
+
+        [Fact]
+        public void CacheExpiresPages() {
+            using (var api = new Api(options, storage)) {
+                using (var config = new Piranha.Config(api)) {
+                    Assert.Equal(0, config.CacheExpiresPages);
+
+                    config.CacheExpiresPages = 30;
+
+                    Assert.Equal(30, config.CacheExpiresPages);
+                }
+            }
+        }
+    }
+}
