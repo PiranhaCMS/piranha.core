6ee4f31c69e2df0931231fb1c7e2d942359e2f13
Håkan Edling <hakan@tidyui.com>
2017-05-08 11:46:47 +0200

    Added some more field tests.

--- test/Piranha.Tests/Fields.cs
+++ test/Piranha.Tests/Fields.cs
@@ -9,6 +9,7 @@
  */
 
 using Piranha.Extend;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -16,11 +17,16 @@ namespace Piranha.Tests
 {
     public class Fields : BaseTests
     {
-        [Field(Name = "First")]
+        [Field(Name = "First", Shorthand = "1")]
         public class MyFirstField : Extend.Fields.SimpleField<string> { }
 
         [Field(Name = "Second")]
         public class MySecondField : Extend.Fields.SimpleField<string> { }
+
+        [Field(Name = "Third")]
+        public class MyThirdField : Extend.Fields.SimpleField<string> { }
+
+        private FieldInfoList fields = new FieldInfoList();
 
         /// <summary>
         /// Sets up & initializes the tests.
@@ -29,6 +35,9 @@ namespace Piranha.Tests
             using (var api = new Api(options, storage)) {
                 Piranha.App.Init(api);
                 Piranha.App.Fields.Register<MyFirstField>();
+
+                fields.Register<MyFirstField>();
+                fields.Register<MyThirdField>();
             }
         }
 
@@ -47,6 +56,14 @@ namespace Piranha.Tests
         }
 
         [Fact]
+        public void GetByTypeLocal() {
+            var field = fields.GetByType(typeof(MyFirstField));
+
+            Assert.NotNull(field);
+            Assert.Equal("First", field.Name);
+        }
+
+        [Fact]
         public void GetByTypeName() {
             var field = Piranha.App.Fields.GetByType(typeof(Extend.Fields.HtmlField).FullName);
 
@@ -55,11 +72,27 @@ namespace Piranha.Tests
         }
 
         [Fact]
+        public void GetByTypeNameLocal() {
+            var field = fields.GetByType(typeof(MyFirstField).FullName);
+
+            Assert.NotNull(field);
+            Assert.Equal("First", field.Name);
+        }
+
+        [Fact]
         public void GetByShorthand() {
             var field = Piranha.App.Fields.GetByShorthand("Html");
 
             Assert.NotNull(field);
             Assert.Equal("Html", field.Name);
+        }
+
+        [Fact]
+        public void GetByShorthandLocal() {
+            var field = fields.GetByShorthand("1");
+
+            Assert.NotNull(field);
+            Assert.Equal("First", field.Name);
         }
 
         [Fact]
@@ -72,6 +105,15 @@ namespace Piranha.Tests
         }
 
         [Fact]
+        public void RegisterLocal() {
+            var count = fields.Count();
+
+            fields.Register<MySecondField>();
+
+            Assert.Equal(count + 1, fields.Count());
+        }
+
+        [Fact]
         public void UnRegister() {
             var count = Piranha.App.Fields.Count();
 
@@ -81,10 +123,90 @@ namespace Piranha.Tests
         }
 
         [Fact]
+        public void UnRegisterLocal() {
+            var count = fields.Count();
+
+            fields.UnRegister<MyThirdField>();
+
+            Assert.Equal(count - 1, fields.Count());
+        }
+
+        [Fact]
         public void Enumerate() {
             foreach (var field in Piranha.App.Fields) {
                 Assert.NotNull(field);
             }
         }
+
+        [Fact]
+        public void EnumerateLocal() {
+            foreach (var field in fields) {
+                Assert.NotNull(field);
+            }
+        }
+
+        [Fact]
+        public void DateFieldConversions() {
+            var now = DateTime.Now;
+
+            Piranha.Extend.Fields.DateField field = now;
+            Assert.Equal(now, field.Value);
+        }
+
+        [Fact]
+        public void HtmlFieldConversions() {
+            var str = "<p>This is a paragraph</p>";
+
+            Piranha.Extend.Fields.HtmlField field = str;
+            Assert.Equal(str, field.Value);
+
+            string output = field;
+            Assert.Equal(str, output);
+        }
+
+        [Fact]
+        public void ImageFieldConversionsNullImage() {
+            var id = Guid.NewGuid().ToString();
+
+            Piranha.Extend.Fields.ImageField field = id;
+            Assert.Equal(id, field.Id);
+
+            string url = field;
+            Assert.Equal("", url);
+        }
+
+        [Fact]
+        public void MarkdownFieldConversions() {
+            var md = "This is a paragraph";
+            var html = Piranha.App.Markdown.Transform(md);
+
+            Piranha.Extend.Fields.MarkdownField field = md;
+            Assert.Equal(md, field.Value);
+
+            string outputHtml = field;
+            Assert.Equal(html, outputHtml);
+        }
+
+        [Fact]
+        public void StringFieldConversions() {
+            var inStr = "This is a string";
+
+            Piranha.Extend.Fields.StringField field = inStr;
+            Assert.Equal(inStr, field.Value);
+
+            string outStr = field;
+            Assert.Equal(inStr, outStr); 
+        }
+
+        [Fact]
+        public void TextFieldConversions() {
+            var inStr = "This is a string";
+
+            Piranha.Extend.Fields.TextField field = inStr;
+            Assert.Equal(inStr, field.Value);
+
+            string outStr = field;
+            Assert.Equal(inStr, outStr); 
+        }        
     }
 }
