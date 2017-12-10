017ee726598b47d779e0f50d0764a9d26dd9f7c3
Håkan Edling <hakan@tidyui.com>
2017-12-10 21:29:58 +0100

    Initialize dynamically inserted html editors. FIxes #152

--- core/Piranha.Manager/assets/js/script.js
+++ core/Piranha.Manager/assets/js/script.js
@@ -14444,6 +14444,10 @@ $(document).on('click', '.btn-media-clear', function () {
     piranha.media.remove($(this));
 });
 
+$('.modal').on('shown.bs.modal', function (event) {
+    $(this).find('input[autofocus]').focus();
+});
+
 $('#modalMedia').on('show.bs.modal', function (event) {
     piranha.media.init($(event.relatedTarget));
     piranha.media.load($(event.relatedTarget), '');
@@ -14701,6 +14705,12 @@ var manager = {
                 }),
                 success: function (res) {
                     $(targetId).append(res);
+
+                    // If the new region contains a html editor, make sure
+                    // we initialize it.
+                    var editors = $(res).find('.editor').each(function () {
+                        tinyMCE.execCommand('mceAddEditor', false, this.id);
+                    });
 
                     if (cb)
                         cb();
