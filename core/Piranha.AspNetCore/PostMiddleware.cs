d2546ab3f12be7fce358945ebd25a06ab674bcde
Håkan Edling <hakan@tidyui.com>
2018-05-27 07:27:35 +0200

    Updated code formatting for Piranha.AspNetCore. See #216

--- core/Piranha.AspNetCore/PostMiddleware.cs
+++ core/Piranha.AspNetCore/PostMiddleware.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017 Håkan Edling
+ * Copyright (c) 2016-2018 Håkan Edling
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
@@ -32,67 +32,83 @@ namespace Piranha.AspNetCore
         /// <param name="context">The current http context</param>
         /// <param name="api">The current api</param>
         /// <returns>An async task</returns>
-        public override async Task Invoke(HttpContext context, IApi api) {
-            if (!IsHandled(context) && !context.Request.Path.Value.StartsWith("/manager/assets/")) {
+        public override async Task Invoke(HttpContext context, IApi api)
+        {
+            if (!IsHandled(context) && !context.Request.Path.Value.StartsWith("/manager/assets/"))
+            {
                 var url = context.Request.Path.HasValue ? context.Request.Path.Value : "";
                 var siteId = GetSiteId(context);
                 var authorized = true;
 
                 var response = PostRouter.Invoke(api, url, siteId);
-                if (response != null) {
-                    if (logger != null)
-                        logger.LogInformation($"Found post\n  Route: {response.Route}\n  Params: {response.QueryString}");
+                if (response != null)
+                {
+                    if (_logger != null)
+                        _logger.LogInformation($"Found post\n  Route: {response.Route}\n  Params: {response.QueryString}");
 
-                    if (!response.IsPublished) {
-                        if (!context.User.HasClaim(Security.Permission.PostPreview, Security.Permission.PostPreview)) {
-                            if (logger != null) {
-                                logger.LogInformation($"User not authorized to preview unpublished posts");
-                            }
+                    if (!response.IsPublished)
+                    {
+                        if (!context.User.HasClaim(Security.Permission.PostPreview, Security.Permission.PostPreview))
+                        {
+                            if (_logger != null)
+                                _logger.LogInformation($"User not authorized to preview unpublished posts");
                             authorized = false;
                         }
                     }
 
-                    if (authorized) {
-                        using (var config = new Config(api)) {
+                    if (authorized)
+                    {
+                        using (var config = new Config(api))
+                        {
                             var headers = context.Response.GetTypedHeaders();
                             var expires = config.CacheExpiresPosts;
 
                             // Only use caching for published posts
-                            if (response.IsPublished && expires > 0) {
-                                if (logger != null)
-                                    logger.LogInformation("Caching enabled. Setting MaxAge, LastModified & ETag");
+                            if (response.IsPublished && expires > 0)
+                            {
+                                if (_logger != null)
+                                    _logger.LogInformation("Caching enabled. Setting MaxAge, LastModified & ETag");
 
-                                headers.CacheControl = new CacheControlHeaderValue() {
+                                headers.CacheControl = new CacheControlHeaderValue
+                                {
                                     Public = true,
                                     MaxAge = TimeSpan.FromMinutes(expires),
                                 };
 
                                 headers.Headers["ETag"] = response.CacheInfo.EntityTag;
                                 headers.LastModified = response.CacheInfo.LastModified;
-                            } else {
-                                headers.CacheControl = new CacheControlHeaderValue() {
+                            }
+                            else
+                            {
+                                headers.CacheControl = new CacheControlHeaderValue
+                                {
                                     NoCache = true
                                 };
                             }
                         }
 
-                        if (HttpCaching.IsCached(context, response.CacheInfo)) {
-                            if (logger != null)
-                                logger.LogInformation("Client has current version. Returning NotModified");
+                        if (HttpCaching.IsCached(context, response.CacheInfo))
+                        {
+                            if (_logger != null)
+                                _logger.LogInformation("Client has current version. Returning NotModified");
 
                             context.Response.StatusCode = 304;
                             return;
-                        } else {
+                        }
+                        else
+                        {
                             context.Request.Path = new PathString(response.Route);
 
-                            if (context.Request.QueryString.HasValue) {
+                            if (context.Request.QueryString.HasValue)
+                            {
                                 context.Request.QueryString = new QueryString(context.Request.QueryString.Value + "&" + response.QueryString);
-                            } else context.Request.QueryString = new QueryString("?" + response.QueryString);
+                            }
+                            else context.Request.QueryString = new QueryString("?" + response.QueryString);
                         }
                     }
                 }
             }
-            await next.Invoke(context);
+            await _next.Invoke(context);
         }
     }
 }
