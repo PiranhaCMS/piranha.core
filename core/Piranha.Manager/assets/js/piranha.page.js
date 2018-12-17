42ed7bda5747fa81a6efcad7b060f1fad15c5aaa
Håkan Edling <hakan@tidyui.com>
2018-12-17 12:21:53 +0100

    Reformatted page & post javascript for the manager.

--- core/Piranha.Manager/assets/js/piranha.page.js
+++ core/Piranha.Manager/assets/js/piranha.page.js
@@ -7,34 +7,40 @@
 // http://github.com/piranhacms/piranha.core
 // 
 
-if (typeof(piranha)  == 'undefined')
+/*global
+    piranha, baseUrl
+ */
+
+if (typeof(piranha)  == "undefined") {
     piranha = {};
+}
 
 piranha.page = new function() {
-    'use strict';
+    "use strict";
 
     var self = this;
 
-    self.pageId = '';
-    self.pageTitle = '';
-    self.siteId = '';
-    self.pageUrlId = '';
+    self.pageId = "";
+    self.pageTitle = "";
+    self.siteId = "";
+    self.pageUrlId = "";
     self.callback = null;
 
     self.init = function (e) {
-        self.pageId = e.data('pageid');
-        self.pageTitle = e.data('pagetitle');
-        self.siteId = e.data('siteid');
+        self.pageId = e.data("pageid");
+        self.pageTitle = e.data("pagetitle");
+        self.siteId = e.data("siteid");
     };
 
     self.load = function (e, site) {
-        if (!site)
+        if (!site) {
             site = self.siteId;
+        }
 
         $.ajax({
-            url: baseUrl + 'manager/page/modal' + (site ? '/' + site : ''),
+            url: baseUrl + "manager/page/modal" + (site ? "/" + site : ""),
             success: function (data) {
-                $('#modalPage .modal-body').html(data);
+                $("#modalPage .modal-body").html(data);
             }
         });
     };
@@ -42,55 +48,55 @@ piranha.page = new function() {
     self.set = function (e) {
         if (!self.callback) {
             if (self.pageUrlId) {
-                $('#' + self.pageUrlId).val(e.data('url'));
+                $("#" + self.pageUrlId).val(e.data("url"));
             } else {
                 if (self.pageId)
-                    $('#' + self.pageId).val(e.data('id'));
-                $('#' + self.pageTitle).text(e.data('title'));
+                    $("#" + self.pageId).val(e.data("id"));
+                $("#" + self.pageTitle).text(e.data("title"));
             }
         } else {
             self.callback({
-                id:  e.data('id'),
-                title: e.data('title'),
-                url: e.data('url')
+                id:  e.data("id"),
+                title: e.data("title"),
+                url: e.data("url")
             });
             self.callback = null;
         }
     };
 
     self.remove = function (e) {
-        $('#' + self.pageId).val('');
-        $('#' + self.pageTitle).text('');
+        $("#" + self.pageId).val("");
+        $("#" + self.pageTitle).text("");
     };
 };
 
-$(document).on('click', '#modalPage .modal-body a', function () {
+$(document).on("click", "#modalPage .modal-body a", function () {
     var button = $(this);
 
-    if (button.data('type') == 'reload') {
-        piranha.page.load(button, button.data('siteid'));
+    if (button.data("type") == "reload") {
+        piranha.page.load(button, button.data("siteid"));
     } else {
         piranha.page.set(button);
-        $('#modalPage').modal('hide');
+        $("#modalPage").modal("hide");
     }
     return false;
 });
 
-$(document).on('submit', '#modalPage form', function (e) {
+$(document).on("submit", "#modalPage form", function (e) {
     e.preventDefault();
 
-    var form = $('#modalPage form');
+    var form = $("#modalPage form");
     var formData = new FormData(form.get(0));
 
     $.ajax({
-        url: $(this).attr('action'),
-        type: 'POST',
+        url: $(this).attr("action"),
+        type: "POST",
         data: formData,
         contentType: false,       
         cache: false,             
         processData: false,                                  
         success: function (data) {
-            $('#modalPage .modal-body').html(data);
+            $("#modalPage .modal-body").html(data);
         },
         error: function (a, b, c) {
             console.log(a)
@@ -100,12 +106,12 @@ $(document).on('submit', '#modalPage form', function (e) {
     }); 
 });
 
-$(document).on('click', '.btn-page-clear', function () {
+$(document).on("click", ".btn-page-clear", function () {
     piranha.page.init($(this));
     piranha.page.remove($(this));
 });
 
-$('#modalPage').on('show.bs.modal', function (event) {
+$("#modalPage").on("show.bs.modal", function (event) {
     piranha.page.init($(event.relatedTarget));
     piranha.page.load($(event.relatedTarget));
 });
