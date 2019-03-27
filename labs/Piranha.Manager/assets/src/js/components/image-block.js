8fe0d1a3a5e0e865de226d59aaea2a0eb3dcded3
Filip <filip.jansson@outlook.com>
2019-03-27 13:35:51 +0100

    Added the use of mediapicker in the image-block component

--- labs/Piranha.Manager/assets/src/js/components/image-block.js
+++ labs/Piranha.Manager/assets/src/js/components/image-block.js
@@ -1,11 +1,37 @@
 Vue.component("image-block", {
     props: ["block"],
+    methods: {
+        clear: function () {
+            // clear media from block
+        },
+        select: function () {
+            piranha.mediapicker.open(this.update);
+        },
+        update: function (media) {
+            if (media.type === "Image") {
+                console.log(this)
+                this.block.body.id = media.id;
+                this.block.body.media = media;
+            } else {
+                console.log("No image was selected");
+            }
+        }
+    },
+    computed: {
+        mediaUrl: function () {
+            if (this.block.body.media.publicUrl.startsWith("~")) {
+                return this.block.body.media.publicUrl.substring(1)
+            } else {
+                return this.block.body.media.publicUrl;
+            }
+        }
+    },
     template:
         "<div>" +
-        "  <img :src='block.body.media.publicUrl.substring(1)'>" +
+        "  <img :src='mediaUrl'>" +
         "  <div class='media-picker'>" +
         "    <div class='btn-group float-right'>" +
-        "      <button class='btn btn-primary text-center'>" +
+        "      <button v-on:click.prevent='select' class='btn btn-primary text-center'>" +
         "        <i class='fas fa-plus'></i>" +
         "      </button>" +
         "      <button class='btn btn-danger text-center'>" +
