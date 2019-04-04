ad678a68e0f2e5724ef3b10d111677adbe78f7b0
Håkan Edling <hakan@tidyui.com>
2019-04-04 09:05:41 +0200

    Restyled modals to panel look.

--- labs/Piranha.Manager/assets/src/js/components/region.js
+++ labs/Piranha.Manager/assets/src/js/components/region.js
@@ -1,7 +1,7 @@
 Vue.component("region", {
     props: ["model"],
     template:
-        "<div class='region row' v-if='!model.meta.isCollection'>" +
+        "<div class='row' v-if='!model.meta.isCollection'>" +
         "  <div class='col-sm-12' v-if='model.meta.description != null'>" +
         "    <div class='alert alert-info'>" +
         "      {{ model.meta.description }}" +
@@ -12,20 +12,24 @@ Vue.component("region", {
         "    <component v-if='field.model != null' v-bind:is='field.meta.component' v-bind:uid='field.meta.uid' v-bind:meta='field.meta' v-bind:model='field.model'></component>" +
         "  </div>" +
         "</div>" +
-        "<div class='list-group region-list' v-else>" +
+        "<div v-else>" +
         "  <div v-if='model.meta.description != null'>" +
         "    <div class='alert alert-info'>" +
         "      {{ model.meta.description }}" +
         "    </div>" +
         "  </div>" +
-        "  <div class='list-group-item region-list-item' v-for='item in model.items'>" +
-        "    <div class='region-list-item-title'>" +
-        "      <a data-toggle='collapse'>List Item Title</a>" +
-        "    </div>" +
-        "    <div class='row'>" +
-        "      <div class='form-group' :class='{ \"col-sm-6\": field.meta.isHalfWidth, \"col-sm-12\": !field.meta.isHalfWidth }' v-for='field in item.fields'>" +
-        "        <label>{{ field.meta.name }}</label>" +
-        "        <component v-if='field.model != null' v-bind:is='field.meta.component' v-bind:uid='field.meta.uid' v-bind:meta='field.meta' v-bind:model='field.model'></component>" +
+        "  <div class='accordion'>" +
+        "    <div class='card' v-for='item in model.items'>" +
+        "      <div class='card-header'>" +
+        "        <a data-toggle='collapse'>List Item Title</a>" +
+        "      </div>" +
+        "      <div class='card-body'>" +
+        "        <div class='row'>" +
+        "          <div class='form-group' :class='{ \"col-sm-6\": field.meta.isHalfWidth, \"col-sm-12\": !field.meta.isHalfWidth }' v-for='field in item.fields'>" +
+        "            <label>{{ field.meta.name }}</label>" +
+        "            <component v-if='field.model != null' v-bind:is='field.meta.component' v-bind:uid='field.meta.uid' v-bind:meta='field.meta' v-bind:model='field.model'></component>" +
+        "          </div>" +
+        "        </div>" +
         "      </div>" +
         "    </div>" +
         "  </div>" +
