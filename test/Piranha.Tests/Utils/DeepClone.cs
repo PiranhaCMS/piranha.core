c1eeab87ba73e19f2a1afd894e14588dbb83b563
Stefan Holm Olsen <stefan@stefanolsen.com>
2019-04-17 13:25:44 +0400

    Add unit tests for the DeepClone method.

---
+++ test/Piranha.Tests/Utils/DeepClone.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Piranha.Models;
+using Xunit;
+
+namespace Piranha.Tests.Utils
+{
+    public class DeepClone
+    {
+        [Fact]
+        public void WithBoolean()
+        {
+            bool parameter = true;
+
+            bool result = Piranha.Utils.DeepClone(parameter);
+            parameter = false;
+
+            // Result should be true and changing 'parameter' should not affect 'result'.
+            Assert.True(result);
+            Assert.NotEqual(parameter, result);
+        }
+
+        [Fact]
+        public void WithAliasNull()
+        {
+            Alias alias = null;
+
+            Alias result = Piranha.Utils.DeepClone(alias);
+            alias = new Alias();
+
+            // Result should be null and not referentially equal to 'parameter'.
+            Assert.Null(result);
+            Assert.NotEqual(alias, result);
+        }
+
+        [Fact]
+        public void WithAliasObject()
+        {
+            Alias alias = new Alias();
+
+            Alias result = Piranha.Utils.DeepClone<Alias>(alias);
+            alias.Id = Guid.NewGuid();
+
+            // Result should not be null true and not referentially equal to 'parameter'.
+            Assert.NotNull(result);
+            Assert.NotEqual(alias, result);
+        }
+    }
+}
