2d22c2637fe517dc2b15c6e921ec7acf75239a7b
Håkan Edling <hakan@tidyui.com>
2019-08-09 12:25:50 +0200

    Added image dialog to Tiny MCE

--- core/Piranha.Manager/assets/dist/js/piranha.js
+++ core/Piranha.Manager/assets/dist/js/piranha.js
@@ -26398,19 +26398,33 @@ piranha.editor = {
     addInline: function (id) {
         tinymce.init({
             selector: "#" + id,
-            //fixed_toolbar_container: "#" + id,
             menubar: false,
             branding: false,
             statusbar: false,
             inline: true,
             convert_urls: false,
             plugins: [
-                "autoresize autolink code hr paste lists piranhalink" // TODO: piranhaimage
+                "autoresize autolink code hr paste lists piranhalink piranhaimage"
             ],
             width: "100%",
             autoresize_min_height: 0,
-            toolbar: "bold italic | bullist numlist hr | alignleft aligncenter alignright | formatselect | piranhalink", // TODO: | piranhalink piranhaimage",
-            block_formats: 'Paragraph=p;Header 1=h1;Header 2=h2;Header 3=h3;Header 4=h4;Code=pre;Quote=blockquote'
+            toolbar: "bold italic | bullist numlist hr | alignleft aligncenter alignright | formatselect | piranhalink piranhaimage",
+            block_formats: 'Paragraph=p;Header 1=h1;Header 2=h2;Header 3=h3;Header 4=h4;Code=pre;Quote=blockquote',
+            file_picker_callback: function(callback, value, meta) {
+                // Provide file and text for the link dialog
+                if (meta.filetype == 'file') {
+                    piranha.mediapicker.openCurrentFolder(function (data) {
+                        callback(data.publicUrl, { text: data.filename });
+                    }, null);
+                }
+
+                // Provide image and alt text for the image dialog
+                if (meta.filetype == 'image') {
+                    piranha.mediapicker.openCurrentFolder(function (data) {
+                        callback(data.publicUrl, { alt: "" });
+                    }, "image");
+                }
+            }
         });
     },
     addInlineMarkdown: function (id, value, update) {
