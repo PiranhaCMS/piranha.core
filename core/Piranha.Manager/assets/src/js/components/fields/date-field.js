31fd80151e84c51b7228251323e7ab2a72f7ebe9
Filip <filip.jansson@outlook.com>
2019-08-09 13:33:22 +0200

    Added datepicker styling and options

--- core/Piranha.Manager/assets/src/js/components/fields/date-field.js
+++ core/Piranha.Manager/assets/src/js/components/fields/date-field.js
@@ -1,5 +1,20 @@
 Vue.component("date-field", {
     props: ["uid", "model", "meta"],
+<<<<<<< Updated upstream
     template:
         "<input class='form-control' type='text' :placeholder='meta.placeholder' v-model='model.value'>"
+=======
+    template: "<datepicker v-model='model.value' :format='_options.format' :monday-first='_options.mondayFirst' :typeable='_options.typeable' :bootstrap-styling='_options.bootstrapStyling'></datepicker>",
+    components: {
+        datepicker: vuejsDatepicker
+    },
+    created: function () {
+        this._options = {
+            bootstrapStyling = true,
+            mondayFirst = true,
+            format = "yyyy-MM-dd",
+            typeable = true
+        };
+    }
+>>>>>>> Stashed changes
 });
