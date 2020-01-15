28cabf1839c6f4813a092a8fb8ad3d8ed5f5cdab
Håkan Edling <hakan@tidyui.com>
2020-01-15 09:12:21 +0100

    Fixed null reference exception in SimpleField

--- core/Piranha/Extend/Fields/SimpleField.cs
+++ core/Piranha/Extend/Fields/SimpleField.cs
@@ -3,9 +3,9 @@
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
- * 
+ *
  * https://github.com/piranhacms/piranha.core
- * 
+ *
  */
 
 using System;
@@ -30,7 +30,7 @@ namespace Piranha.Extend.Fields
         /// </summary>
         public override string GetTitle()
         {
-            if (Value.Equals(default(T)))
+            if (Value == null)
             {
                 return null;
             }
