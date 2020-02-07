54857bdadd78c1e519d96ba1961e6be2c05fc600
Lukas Nordin <lukas.nordin@bybrick.se>
2020-02-07 14:57:01 +0100

    Compile .vue templates into components

--- core/Piranha.Manager/gulpfile.js
+++ core/Piranha.Manager/gulpfile.js
@@ -8,7 +8,86 @@
  * https://github.com/piranhacms/piranha.core
  *
  */
+// Gulp task to compile .vue files into Vue.Components(...)
+var path = require('path'),
+    vueCompiler = require('vue-template-compiler'),
+    babel = require("@babel/core"),
+    babelTemplate = require("@babel/template").default,
+    codeFrameColumns = require('@babel/code-frame').codeFrameColumns,
+    babelTypes = require("@babel/types"),
+    through2 = require('through2');
 
+function vueCompile() {
+    return through2.obj(function (file, _, callback) {
+        var relativeFile = path.relative(file.cwd, file.path);
+        var ext = path.extname(file.path);
+        if (ext === '.vue') {
+            function getComponent(ast, sourceCode) {
+                const ta = ast.program.body[0]
+                if (!babelTypes.isExportDefaultDeclaration(ta)) {
+                    var msg = 'Top level declation in file ' + relativeFile + ' must be "export default {" \n' + codeFrameColumns(sourceCode, { start: ta.loc.start }, { highlightCode: true });
+                    throw msg;
+                }
+                return ta.declaration;
+            }
+
+            function compile(componentName, content) {
+                var component = vueCompiler.parseComponent(content, []);
+                if (component.styles.length > 0) {
+                    component.styles.forEach(s => {
+                        const linesToStyle = content.substr(0, s.start).split(/\r?\n/).length;
+                        var msg = 'WARNING: <style> tag in ' + relativeFile + ' is ignored\n' + codeFrameColumns(content, { start: { line: linesToStyle } }, { highlightCode: true });
+                        console.warn(msg);
+                    });
+                }
+
+                var ast = babel.parse(component.script.content, {
+                    parserOpts: {
+                        sourceFilename: file.path
+                    }
+                });
+
+                var vueComponent = getComponent(ast, component.script.content);
+                vueComponent.properties.push(babelTypes.objectProperty(babelTypes.identifier('template'), babelTypes.stringLiteral(component.template.content)))
+
+                var wrapInComponent = babelTemplate("Vue.component(NAME, COMPONENT);");
+                var componentAst = wrapInComponent({
+                    NAME: babelTypes.stringLiteral(componentName),
+                    COMPONENT: vueComponent
+                })
+
+                ast.program.body = [componentAst]
+
+                babel.transformFromAst(ast, null, null, function (err, result) {
+                    if (err) {
+                        callback(err, null)
+                    }
+                    else {
+                        file.contents = Buffer.from(result.code);
+                        callback(null, file)
+                    }
+                });
+            }
+            var componentName = path.basename(file.path, ext);
+            if (file.isBuffer()) {
+                compile(componentName, file.contents.toString());
+            }
+            else if (file.isStream()) {
+                var chunks = [];
+                file.contents.on('data', function (chunk) {
+                    chunks.push(chunk);
+                });
+                file.contents.on('end', function () {
+                    compile(componentName, Buffer.concat(chunks).toString());
+                });
+            }
+        } else {
+            callback(null, file);
+        }
+    });
+}
+
+// Gulp build script
 var gulp = require("gulp"),
     sass = require('gulp-sass'),
     concat = require("gulp-concat"),
@@ -218,6 +297,7 @@ gulp.task("min:js", function () {
     for (var n = 0; n < js.length; n++)
     {
         gulp.src(js[n].items, { base: "." })
+            .pipe(vueCompile())
             .pipe(concat(output + "js/" + js[n].name))
             .pipe(gulp.dest("."))
             .pipe(uglify().on('error', function (e) {
