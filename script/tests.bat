504792d25d4dfcaa1e90f167df636d67433b2891
Håkan Edling <hakan@tidyui.com>
2020-02-07 21:42:53 +0100

    Added manager tests to test .bat-file

--- script/tests.bat
+++ script/tests.bat
@@ -1,3 +1,4 @@
-dotnet test test/Piranha.Tests/Piranha.Tests.csproj
 dotnet test test/Piranha.AttributeBuilder.Tests/Piranha.AttributeBuilder.Tests.csproj
 dotnet test test/Piranha.ImageSharp.Tests/Piranha.ImageSharp.Tests.csproj
+dotnet test test/Piranha.Manager.Tests/Piranha.Manager.Tests.csproj
+dotnet test test/Piranha.Tests/Piranha.Tests.csproj
