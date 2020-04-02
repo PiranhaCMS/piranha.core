d6777eaf566c7c722c977fd6474566b66d327a19
Håkan Edling <hakan@tidyui.com>
2020-04-02 09:36:46 +0200

    Updated build scripts

--- build.sh
+++ build.sh
@@ -35,6 +35,4 @@ dotnet build
 
 # Test
 echo "Starting tests..."
-dotnet test ./test/Piranha.AttributeBuilder.Tests/Piranha.AttributeBuilder.Tests.csproj
-dotnet test ./test/Piranha.ImageSharp.Tests/Piranha.ImageSharp.Tests.csproj
 dotnet test ./test/Piranha.Tests/Piranha.Tests.csproj
