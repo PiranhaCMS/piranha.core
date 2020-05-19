e421e4d76c043244ea9ac9fadf659438e53870c4
Filip <filip.jansson@outlook.com>
2020-05-19 11:07:58 +0200

    Added page information migration for sql server

---
+++ data/Piranha.Data.EF.SQLServer/Migrations/20200519090245_AddPageInformation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Piranha.Data.EF.SQLServer.Migrations
+{
+    public partial class AddPageInformation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "EnableComments",
+                table: "Piranha_Posts",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Excerpt",
+                table: "Piranha_Pages",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PrimaryImageId",
+                table: "Piranha_Pages",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Excerpt",
+                table: "Piranha_Pages");
+
+            migrationBuilder.DropColumn(
+                name: "PrimaryImageId",
+                table: "Piranha_Pages");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "EnableComments",
+                table: "Piranha_Posts",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldDefaultValue: false);
+        }
+    }
+}
