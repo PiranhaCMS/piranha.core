ec2d9b9f7a17f041f3e04109d76c7017f30e8416
Håkan Edling <hakan@tidyui.com>
2020-05-31 08:34:49 +0200

    Added MySQL & PostgreSQL identity providers to core repository.

---
+++ core/Piranha.AspNetCore.Identity.MySQL/DbFactory.cs
@@ -0,0 +1,37 @@
+﻿#if DEBUG
+/*
+ * Copyright (c) 2019 aatmmr
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ * 
+ * https://github.com/piranhacms/piranha.core.mysql
+ * 
+ */
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Piranha.AspNetCore.Identity.MySQL
+{
+    /// <summary>
+    /// Factory for creating a db context. Only used in dev mode
+    /// when creating migrations.
+    /// </summary>
+    [NoCoverage]
+    public class DbFactory : IDesignTimeDbContextFactory<IdentityMySQLDb>
+    {
+        /// <summary>
+        /// Creates a new db context.
+        /// </summary>
+        /// <param name="args">The arguments</param>
+        /// <returns>The db context</returns>
+        public IdentityMySQLDb CreateDbContext(string[] args) 
+        {
+            var builder = new DbContextOptionsBuilder<IdentityMySQLDb>();
+            builder.UseMySql("server=localhost;port=3306;database=piranha;uid=root;password=password");
+            return new IdentityMySQLDb(builder.Options);
+        }
+    }
+}
+#endif
---
+++ core/Piranha.AspNetCore.Identity.MySQL/Migrations/20180605110313_InitialCreate.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Piranha.AspNetCore.Identity.MySQL.Migrations
+{
+    [NoCoverage]
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Piranha_Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Piranha_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Piranha_Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    NormalizedEmail = table.Column<string>(maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(maxLength: 256, nullable: true),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Piranha_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Piranha_RoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true),
+                    RoleId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Piranha_RoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Piranha_RoleClaims_Piranha_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Piranha_Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Piranha_UserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Piranha_UserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Piranha_UserClaims_Piranha_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Piranha_Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Piranha_UserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(nullable: false),
+                    ProviderKey = table.Column<string>(nullable: false),
+                    ProviderDisplayName = table.Column<string>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Piranha_UserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_Piranha_UserLogins_Piranha_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Piranha_Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Piranha_UserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(nullable: false),
+                    RoleId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Piranha_UserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_Piranha_UserRoles_Piranha_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Piranha_Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Piranha_UserRoles_Piranha_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Piranha_Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Piranha_UserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(nullable: false),
+                    LoginProvider = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Piranha_UserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_Piranha_UserTokens_Piranha_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Piranha_Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Piranha_RoleClaims_RoleId",
+                table: "Piranha_RoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "Piranha_Roles",
+                column: "NormalizedName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Piranha_UserClaims_UserId",
+                table: "Piranha_UserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Piranha_UserLogins_UserId",
+                table: "Piranha_UserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Piranha_UserRoles_RoleId",
+                table: "Piranha_UserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "Piranha_Users",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "Piranha_Users",
+                column: "NormalizedUserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Piranha_RoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "Piranha_UserClaims");
+
+            migrationBuilder.DropTable(
+                name: "Piranha_UserLogins");
+
+            migrationBuilder.DropTable(
+                name: "Piranha_UserRoles");
+
+            migrationBuilder.DropTable(
+                name: "Piranha_UserTokens");
+
+            migrationBuilder.DropTable(
+                name: "Piranha_Roles");
+
+            migrationBuilder.DropTable(
+                name: "Piranha_Users");
+        }
+    }
+}
