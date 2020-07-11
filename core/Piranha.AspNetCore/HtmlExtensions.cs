2063dd8662cd9cb5c06b254890475bcd96a0ffc3
Håkan Edling <hakan@tidyui.com>
2020-07-11 16:51:47 +0200

    Added helper for rendering meta-tags. See #1218

---
+++ core/Piranha.AspNetCore/HtmlExtensions.cs
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) .NET Foundation and Contributors
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ * https://github.com/piranhacms/piranha.core
+ *
+ */
+
+using System.Text;
+using Microsoft.AspNetCore.Html;
+using Piranha.AspNetCore.Services;
+using Piranha.Models;
+
+/// <summary>
+/// Extension class with html helper methods.
+/// </summary>
+public static class HtmlExtensions
+{
+    /// <summary>
+    /// Generates meta tags for the given content.
+    /// </summary>
+    /// <param name="app">The application service</param>
+    /// <param name="content">The content</param>
+    /// <param name="meta">If meta tags should be generated</param>
+    /// <param name="opengraph">If open graph tags should be generated</param>
+    /// <param name="generator">If generator tag should be generated</param>
+    /// <returns>The meta tags</returns>
+    public static HtmlString MetaTags(this IApplicationService app, IMeta content, bool meta = true, bool opengraph = true, bool generator = true)
+    {
+        var sb = new StringBuilder();
+
+        if (meta)
+        {
+            // Generate meta tags
+            if (!string.IsNullOrWhiteSpace(content.MetaKeywords))
+            {
+                sb.AppendLine($"<meta name=\"keywords\" value=\"{ content.MetaKeywords }\">");
+            }
+            if (!string.IsNullOrWhiteSpace(content.MetaDescription))
+            {
+                sb.AppendLine($"<meta name=\"description\" value=\"{ content.MetaDescription }\">");
+            }
+        }
+
+        if (generator)
+        {
+            // Generate generator tag
+            sb.AppendLine($"<meta name=\"generator\" value=\"Piranha CMS { Piranha.Utils.GetAssemblyVersion(typeof(Piranha.App).Assembly) }\">");
+        }
+
+        if (opengraph)
+        {
+            // Generate open graph tags
+            sb.AppendLine($"<meta name=\"og:type\" value=\"article\">");
+            sb.AppendLine($"<meta name=\"og:title\" value=\"{ content.Title }\">");
+            if (!string.IsNullOrWhiteSpace(content.MetaDescription))
+            {
+                sb.AppendLine($"<meta name=\"og:description\" value=\"{ content.MetaDescription }\">");
+            }
+        }
+        return new HtmlString(sb.ToString());
+    }
+}
