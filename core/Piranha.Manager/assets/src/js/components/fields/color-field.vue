a2e62e40871815524d00076658b9606f61d0b0b7
Filip <filip.jansson@outlook.com>
2020-12-15 17:33:23 +0100

    Updated color field component to use settings and update title

--- core/Piranha.Manager/assets/src/js/components/fields/color-field.vue
+++ core/Piranha.Manager/assets/src/js/components/fields/color-field.vue
@@ -4,12 +4,26 @@
             <div class="color-preview" :style="{ backgroundColor: model.value }"></div>
             <input class="form-control" type="color" v-model="model.value">
         </div>
-        <input class="form-control" type="text" v-model="model.value" :placeholder="meta.placeholder">
+        <input class="form-control" type="text" v-model="model.value" v-on:change="update()" :readonly="readonly()" :placeholder="meta.placeholder">
     </div>    
 </template>
 
 <script>
 export default {
-    props: ["uid", "model", "meta"]
+    props: ["uid", "model", "meta"],
+        methods: {
+        update: function () {
+            // Tell parent that title has been updated
+            if (this.meta.notifyChange) {
+                this.$emit('update-title', {
+                    uid: this.uid,
+                    title: this.model.value
+                });
+            }
+        },
+        readonly: function () {
+            return this.meta.settings.DisallowInput != null && this.meta.settings.DisallowInput;
+        }
+    }
 }
 </script>
