107416d7ba6dfa79edbbcbcb72056ca0b4d662ec
tedvanderveen <tedvanderveen@vanderveen.net>
2021-03-24 16:29:14 +0100

    Use Azure.Storage.Blobs Nuget packge to replace deprecated Microsoft.Azure.Storage.Blob

--- core/Piranha.Azure.BlobStorage/BlobStorage.cs
+++ core/Piranha.Azure.BlobStorage/BlobStorage.cs
@@ -8,30 +8,22 @@
  *
  */
 
-using Microsoft.Azure.Storage;
-using Microsoft.Azure.Storage.Auth;
-using Microsoft.Azure.Storage.Blob;
+using System;
+using System.IO;
 using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Piranha.Models;
 
 namespace Piranha.Azure
 {
-    public class BlobStorage : IStorage
+    public class BlobStorage : IStorage, IStorageSession
     {
         /// <summary>
         /// The private storage account.
         /// </summary>
-        private readonly CloudStorageAccount _storage;
-
-        /// <summary>
-        /// The name of the container to use.
-        /// </summary>
-        private readonly string _containerName;
-
-        /// <summary>
-        /// The container url.
-        /// </summary>
-        private string _containerUrl;
+        private readonly BlobContainerClient _storage;
 
         /// <summary>
         /// How uploaded files should be named to
@@ -42,16 +34,18 @@ namespace Piranha.Azure
         /// <summary>
         /// Creates a new Blog Storage service from the given credentials.
         /// </summary>
+        /// <param name="blobContainerUri">
+        /// A <see cref="Uri"/> referencing the blob service.
+        /// This is likely to be similar to "https://{account_name}.blob.core.windows.net".
+        /// </param>
         /// <param name="credentials">The connection credentials</param>
-        /// <param name="containerName">The container name</param>
         /// <param name="naming">How uploaded media files should be named</param>
         public BlobStorage(
-            StorageCredentials credentials,
-            string containerName = "uploads",
+            Uri blobContainerUri,
+            TokenCredential credentials,
             BlobStorageNaming naming = BlobStorageNaming.UniqueFileNames)
         {
-            _storage = new CloudStorageAccount(credentials, true);
-            _containerName = containerName;
+            _storage = new BlobContainerClient(blobContainerUri, credentials);
             _naming = naming;
         }
 
@@ -66,8 +60,7 @@ namespace Piranha.Azure
             string containerName = "uploads",
             BlobStorageNaming naming = BlobStorageNaming.UniqueFileNames)
         {
-            _storage = CloudStorageAccount.Parse(connectionString);
-            _containerName = containerName;
+            _storage = new BlobContainerClient(connectionString, containerName);
             _naming = naming;
         }
 
@@ -77,20 +70,12 @@ namespace Piranha.Azure
         /// <returns>A new open session</returns>
         public async Task<IStorageSession> OpenAsync()
         {
-            var session = _storage.CreateCloudBlobClient();
-            var container = session.GetContainerReference(_containerName);
-
-            if (!await container.ExistsAsync())
+            if (!await _storage.ExistsAsync())
             {
-                await container.CreateAsync();
-                await container.SetPermissionsAsync(new BlobContainerPermissions
-                {
-                    PublicAccess = BlobContainerPublicAccessType.Blob
-                });
+                await _storage.CreateAsync(PublicAccessType.Blob);
             }
-            _containerUrl = container.Uri.AbsoluteUri;
 
-            return new BlobStorageSession(this, container, _naming);
+            return this;
         }
 
         /// <summary>
@@ -103,14 +88,7 @@ namespace Piranha.Azure
         {
             if (media != null && !string.IsNullOrWhiteSpace(id))
             {
-                if (string.IsNullOrEmpty(_containerUrl))
-                {
-                    var session = _storage.CreateCloudBlobClient();
-                    var container = session.GetContainerReference(_containerName);
-
-                    _containerUrl = container.Uri.AbsoluteUri;
-                }
-                return $"{ _containerUrl }/{ GetResourceName(media, id, true) }";
+                return $"{ _storage.Uri.AbsoluteUri }/{ GetResourceName(media, id, true) }";
             }
             return null;
         }
@@ -143,6 +121,81 @@ namespace Piranha.Azure
             {
                 return $"{ media.Id }/{ (encode ? System.Web.HttpUtility.UrlPathEncode(filename) : filename) }";
             }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Writes the content for the specified media content to the given stream.
+        /// </summary>
+        /// <param name="media">The media file</param>
+        /// <param name="filename">The file name</param>
+        /// <param name="stream">The output stream</param>
+        /// <returns>If the media was found</returns>
+        public async Task<bool> GetAsync(Media media, string filename, Stream stream)
+        {
+            var blob = _storage.GetBlobClient(GetResourceName(media, filename));
+
+            if (await blob.ExistsAsync())
+            {
+                await blob.DownloadToAsync(stream);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Stores the given media content.
+        /// </summary>
+        /// <param name="media">The media file</param>
+        /// <param name="filename">The file name</param>
+        /// <param name="contentType">The content type</param>
+        /// <param name="stream">The input stream</param>
+        /// <returns>The public URL</returns>
+        public async Task<string> PutAsync(Media media, string filename, string contentType, Stream stream)
+        {
+            var blob = _storage.GetBlobClient(GetResourceName(media, filename));
+
+            var blobHttpHeader = new BlobHttpHeaders {ContentType = contentType};
+
+            await blob.UploadAsync(stream, blobHttpHeader);
+
+            return blob.Uri.AbsoluteUri;
+        }
+
+        /// <summary>
+        /// Stores the given media content.
+        /// </summary>
+        /// <param name="media">The media file</param>
+        /// <param name="filename">The file name</param>
+        /// <param name="contentType">The content type</param>
+        /// <param name="bytes">The binary data</param>
+        /// <returns>The public URL</returns>
+        public async Task<string> PutAsync(Media media, string filename, string contentType, byte[] bytes)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var writer = new StreamWriter(ms);
+                writer.Write(bytes);
+                writer.Flush();
+                ms.Position = 0;
+                return await PutAsync(media, filename, contentType, ms);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the content for the specified media.
+        /// </summary>
+        /// <param name="media">The media file</param>
+        /// <param name="filename">The file name</param>
+        public async Task<bool> DeleteAsync(Media media, string filename)
+        {
+            var blob = _storage.GetBlobClient(GetResourceName(media, filename));
+
+            return await blob.DeleteIfExistsAsync();
         }
     }
 }
