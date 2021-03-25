dbba1b7809e049a2bdff46f1c74d0e7faaaa455b
tedvanderveen <tedvanderveen@vanderveen.net>
2021-03-25 16:41:29 +0100

    Remove unused `using` refs

--- core/Piranha/IInitializable.cs
+++ core/Piranha/IInitializable.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-
-namespace Piranha
+﻿namespace Piranha
 {
     public interface IInitializable
     {
--- core/Piranha/IStorage.cs
+++ core/Piranha/IStorage.cs
@@ -8,7 +8,6 @@
  *
  */
 
-using System.IO;
 using System.Threading.Tasks;
 using Piranha.Models;
 
