c2f517dd652f1812836ac66e807b092b65a8f764
Filip <filip.jansson@outlook.com>
2021-04-06 13:48:20 +0200

    Added content block migration for SQL Server

--- data/Piranha.Data.EF.SQLServer/Migrations/20210406114503_AddContentBlocks.Designer.cs
+++ data/Piranha.Data.EF.SQLServer/Migrations/20210406114503_AddContentBlocks.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Piranha.Data.EF.SQLServer;
 
 namespace Piranha.Data.EF.SQLServer.Migrations
 {
     [DbContext(typeof(SQLServerDb))]
-    partial class DbModelSnapshot : ModelSnapshot
+    [Migration("20210406114503_AddContentBlocks")]
+    partial class AddContentBlocks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
