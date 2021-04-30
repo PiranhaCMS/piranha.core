0d923d716c4666732d7fab7e9ceefd4b2f9d03f5
Filip <filip.jansson@outlook.com>
2021-04-30 08:15:12 +0200

    Added translation last modified sql server migration

--- data/Piranha.Data.EF.SQLServer/Migrations/20210430061233_AddTranslationLastModified.Designer.cs
+++ data/Piranha.Data.EF.SQLServer/Migrations/20210430061233_AddTranslationLastModified.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Piranha.Data.EF.SQLServer;
 
 namespace Piranha.Data.EF.SQLServer.Migrations
 {
     [DbContext(typeof(SQLServerDb))]
-    partial class DbModelSnapshot : ModelSnapshot
+    [Migration("20210430061233_AddTranslationLastModified")]
+    partial class AddTranslationLastModified
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
