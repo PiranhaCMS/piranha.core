27e289560568cdc4461b103f45cddd4102fa610f
tedvanderveen <tedvanderveen@vanderveen.net>
2021-05-06 10:27:00 +0200

    Move Login and Logoff pages to Piranha.AspNetCore.Identity project

---
+++ identity/Piranha.AspNetCore.Identity/Areas/Manager/Pages/Login.cshtml.cs
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2019 Håkan Edling
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ * https://github.com/piranhacms/piranha.core
+ *
+ */
+
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Piranha.Manager.Models
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ISecurity _service;
+        private readonly ManagerLocalizer _localizer;
+
+        public LoginModel(ISecurity service, ManagerLocalizer localizer)
+        {
+            _service = service;
+            _localizer = localizer;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            public string Username { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+
+        public void OnGet(string returnUrl = null)
+        {
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
+
+            ReturnUrl = returnUrl;
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            await _service.SignOut(HttpContext);
+
+            if (!ModelState.IsValid || !await _service.SignIn(HttpContext, Input.Username, Input.Password))
+            {
+                ModelState.Clear();
+                ModelState.AddModelError(string.Empty, _localizer.General["Username and/or password are incorrect."].Value);
+                return Page();
+            }
+
+            if (!string.IsNullOrEmpty(returnUrl))
+            {
+                return LocalRedirect(returnUrl);
+            }
+
+            return new RedirectToPageResult("Index");
+        }
+    }
+}
