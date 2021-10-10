a81876ea2d32a3e03e0dc67cc24d5ab32e324750
Håkan Edling <hakan@tidyui.com>
2021-10-10 09:52:07 +0200

    Updated summernote & codemirror packages. Fixes #1730

--- core/Piranha.Manager.Summernote/assets/dist/lang/summernote-ar-AR.js
+++ core/Piranha.Manager.Summernote/assets/dist/lang/summernote-ar-AR.js
@@ -1,4 +1,117 @@
-(function($) {
+/*!
+ * 
+ * Super simple wysiwyg editor v0.8.18
+ * https://summernote.org
+ * 
+ * 
+ * Copyright 2013- Alan Hong. and other contributors
+ * summernote may be freely distributed under the MIT license.
+ * 
+ * Date: 2020-05-20T16:47Z
+ * 
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 7);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 7:
+/***/ (function(module, exports) {
+
+(function ($) {
   $.extend($.summernote.lang, {
     'ar-AR': {
       font: {
@@ -43,7 +156,7 @@
         providers: '(YouTube, Vimeo, Vine, Instagram, DailyMotion ou Youku)'
       },
       link: {
-        link: 'رابط رابط',
+        link: 'رابط',
         insert: 'إدراج',
         unlink: 'حذف الرابط',
         edit: 'تعديل',
@@ -53,13 +166,13 @@
       },
       table: {
         table: 'جدول',
-        addRowAbove: 'Add row above',
-        addRowBelow: 'Add row below',
-        addColLeft: 'Add column left',
-        addColRight: 'Add column right',
-        delRow: 'Delete row',
-        delCol: 'Delete column',
-        delTable: 'Delete table'
+        addRowAbove: 'إضافة سطر أعلاه',
+        addRowBelow: 'إضافة سطر أدناه',
+        addColLeft: 'إضافة عمود قبله',
+        addColRight: 'إضافة عمود بعده',
+        delRow: 'حذف سطر',
+        delCol: 'حذف عمود',
+        delTable: 'حذف الجدول'
       },
       hr: {
         insert: 'إدراج خط أفقي'
@@ -112,45 +225,50 @@
         action: 'Action',
         paragraphFormatting: 'تنسيق الفقرة',
         documentStyle: 'تنسيق المستند',
-        extraKeys: 'Extra keys'
+        extraKeys: 'أزرار إضافية'
       },
       help: {
-        'insertParagraph': 'Insert Paragraph',
-        'undo': 'Undoes the last command',
-        'redo': 'Redoes the last command',
-        'tab': 'Tab',
-        'untab': 'Untab',
-        'bold': 'Set a bold style',
-        'italic': 'Set a italic style',
-        'underline': 'Set a underline style',
-        'strikethrough': 'Set a strikethrough style',
-        'removeFormat': 'Clean a style',
-        'justifyLeft': 'Set left align',
-        'justifyCenter': 'Set center align',
-        'justifyRight': 'Set right align',
-        'justifyFull': 'Set full align',
-        'insertUnorderedList': 'Toggle unordered list',
-        'insertOrderedList': 'Toggle ordered list',
-        'outdent': 'Outdent on current paragraph',
-        'indent': 'Indent on current paragraph',
-        'formatPara': 'Change current block\'s format as a paragraph(P tag)',
-        'formatH1': 'Change current block\'s format as H1',
-        'formatH2': 'Change current block\'s format as H2',
-        'formatH3': 'Change current block\'s format as H3',
-        'formatH4': 'Change current block\'s format as H4',
-        'formatH5': 'Change current block\'s format as H5',
-        'formatH6': 'Change current block\'s format as H6',
-        'insertHorizontalRule': 'Insert horizontal rule',
-        'linkDialog.show': 'Show Link Dialog'
+        'insertParagraph': 'إدراج فقرة',
+        'undo': 'تراجع عن آخر أمر',
+        'redo': 'إعادة تنفيذ آخر أمر',
+        'tab': 'إزاحة (تاب)',
+        'untab': 'سحب النص باتجاه البداية',
+        'bold': 'تنسيق عريض',
+        'italic': 'تنسيق مائل',
+        'underline': 'تنسيق خط سفلي',
+        'strikethrough': 'تنسيق خط متوسط للنص',
+        'removeFormat': 'إزالة التنسيقات',
+        'justifyLeft': 'محاذاة لليسار',
+        'justifyCenter': 'محاذاة توسيط',
+        'justifyRight': 'محاذاة لليمين',
+        'justifyFull': 'محاذاة كاملة',
+        'insertUnorderedList': 'قائمة منقّطة',
+        'insertOrderedList': 'قائمة مرقّمة',
+        'outdent': 'إزاحة للأمام على الفقرة الحالية',
+        'indent': 'إزاحة للخلف على الفقرة الحالية',
+        'formatPara': 'تغيير التنسيق للكتلة الحالية إلى فقرة',
+        'formatH1': 'تغيير التنسيق للكتلة الحالية إلى ترويسة 1',
+        'formatH2': 'تغيير التنسيق للكتلة الحالية إلى ترويسة 2',
+        'formatH3': 'تغيير التنسيق للكتلة الحالية إلى ترويسة 3',
+        'formatH4': 'تغيير التنسيق للكتلة الحالية إلى ترويسة 4',
+        'formatH5': 'تغيير التنسيق للكتلة الحالية إلى ترويسة 5',
+        'formatH6': 'تغيير التنسيق للكتلة الحالية إلى ترويسة 6',
+        'insertHorizontalRule': 'إدراج خط أفقي',
+        'linkDialog.show': 'إظهار خصائص الرابط'
       },
       history: {
         undo: 'تراجع',
         redo: 'إعادة'
       },
       specialChar: {
-        specialChar: 'SPECIAL CHARACTERS',
-        select: 'Select Special characters'
+        specialChar: 'محارف خاصة',
+        select: 'اختر المحرف الخاص'
       }
     }
   });
 })(jQuery);
+
+/***/ })
+
+/******/ });
+});
