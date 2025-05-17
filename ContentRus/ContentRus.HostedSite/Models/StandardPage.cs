75ebed6065096273c9965a3ea8542a9b7b8d1001
Migas77 <miguel.belchior@ua.pt>
2025-05-17 11:22:59 +0100

    CMS separated from Manager

---
+++ ContentRus/ContentRus.HostedSite/Models/StandardPage.cs
@@ -0,0 +1,9 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace ContentRus.HostedSite.Models;
+
+[PageType(Title = "Standard page")]
+public class StandardPage  : Page<StandardPage>
+{
+}
