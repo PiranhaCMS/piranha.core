56e57d185cd9ee681e8d56dc7af6cd15d9d5158b
DiogoSilva1904 <diogompsilva2000@gmail.com>
2025-05-29 16:58:10 +0100

    email sending working(using endpoint)

---
+++ ContentRus.ControlPlane/ContentRus.Notifications/NotificationService/Controllers/Test.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Notifications.Services;
+
+[ApiController]
+[Route("api")]
+public class TestEmailController : ControllerBase
+{
+    private readonly EmailService _emailService;
+
+    public TestEmailController(EmailService emailService)
+    {
+        _emailService = emailService;
+    }
+
+    [HttpGet("send")]
+    public async Task<IActionResult> SendTestEmail()
+    {
+        //Put the email to witch you want to send(first argument)
+        await _emailService.SendEmailAsync("", "Welcome to ContentRus", "<h1>Your ContenRus account is ready to be used.</h1><p>Sent from ContentRus.</p>");
+        return Ok("Email sent (if config is correct)");
+    }
+}
