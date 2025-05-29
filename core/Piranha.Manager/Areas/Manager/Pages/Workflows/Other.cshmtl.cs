79b8d08ef55e197b93a790ee95428423e3df4f00
Lia Cardoso <lia.cardoso@ua.pt>
2025-05-29 17:54:51 +0100

    published content apearing in tab Publshid Content

--- core/Piranha.Manager/Areas/Manager/Pages/Workflows/Other.cshmtl.cs
+++ core/Piranha.Manager/Areas/Manager/Pages/Workflows/Other.cshmtl.cs
@@ -1,38 +1,36 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Piranha;
 using Piranha.Manager.Models;
 
 namespace Piranha.Manager.Pages.Workflows
 {
     public class PublishedPagesModel : PageModel
     {
+        private readonly IApi _api;
+
         public List<PageListModel.PageItem> Pages { get; set; } = new();
 
-        public void OnGet()
+        public PublishedPagesModel(IApi api)
         {
-            Pages = new List<PageListModel.PageItem>
-            {
-                new PageListModel.PageItem {
-                    Id = Guid.Parse("b5acfcf0-bf3d-4a11-95a9-7cfba7201e50"),
-                    Title = "Lorem Ipsum Dolor",
-                    TypeName = "BlogPost",
-                    Status = PageListModel.PageItem.Unpublished,
-                    EditUrl = "/manager/page/edit/1"
-                },
-                new PageListModel.PageItem {
-                    Id = Guid.Parse("df29f8ae-4ac6-4e66-bb0f-99c72ebcfe49"),
-                    Title = "Company Update",
-                    TypeName = "NewsItem",
-                    Status = PageListModel.PageItem.Draft,
-                    EditUrl = "/manager/page/edit/2"
-                },
-                new PageListModel.PageItem {
-                    Id = Guid.Parse("fa3bda46-b125-4d7b-9cc7-4d0e08752b62"),
-                    Title = "Tech Trends 2025",
-                    TypeName = "Article",
-                    Status = "Published",
-                    EditUrl = "/manager/page/edit/3"
-                }
-            };
+            _api = api;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var siteId = (await _api.Sites.GetDefaultAsync()).Id;
+            var allPages = await _api.Pages.GetAllAsync(siteId);
+
+            Pages = allPages
+                .Where(p => p.Published.HasValue) // <-- só páginas publicadas
+                .Select(p => new PageListModel.PageItem
+                {
+                    Id       = p.Id,
+                    Title    = p.Title,
+                    TypeName = p.TypeId,
+                    Status   = "Published",
+                    EditUrl  = $"/manager/page/edit/{p.Id}"
+                })
+                .ToList();
         }
     }
 }
